345599487400561bb174191b8710a7cbbeee5e4c
Matt Burstein <j.matthew.burstein@gmail.com>
2019-03-03 21:51:27 +0000

    feat: add HeadingContainer to centre heading

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { HeadingContainer } from './LayoutStyledComponents'
 
 const Heading = () => {
-  return <h1>Tic Tac Toe</h1>
+  return (
+    <HeadingContainer>
+      <h1>Tic Tac Toe</h1>
+    </HeadingContainer>
+  )
 }
 
 export default Heading;
--- src/components/LayoutStyledComponents.js
+++ src/components/LayoutStyledComponents.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+export const HeadingContainer = styled.div`
+  text-align: center;
+`
+
 export const BoardContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
