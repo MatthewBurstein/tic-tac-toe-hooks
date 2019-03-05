3398568ffc56a93ce262e06525f7c2bcc38d161b
Matt Burstein <j.matthew.burstein@gmail.com>
2019-03-05 22:21:49 +0000

    feat: add condition to evaluateBoard to check for empty squares

--- src/logic/evaluateBoard.js
+++ src/logic/evaluateBoard.js
@@ -1,16 +1,16 @@
 const evaluateBoard = board => {
   const rows = [board.slice(0,3), board.slice(3, 6), board.slice(6, 9)]
-  const winners = []
   const rowWinners = evaluateRows(rows)
   const columnWinners = evaluateColumns(rows)
   const diagonalWinners = evaluateDiagonals(rows)
-  return [rowWinners, columnWinners, diagonalWinners].filter(winners => !!winners)
+  const winners = [rowWinners, columnWinners, diagonalWinners].filter(winners => !!winners)
+  return winners
 }
 
 const evaluateRows = rows => {
   let output
   rows.forEach((row, idx) => {
-    if (row.every(el => el === row[0])) {
+    if (row[0] && row.every(el => el === row[0])) {
       output = {
         winner: row[0],
         type: "row",
@@ -22,10 +22,10 @@ const evaluateRows = rows => {
 }
 
 const evaluateColumns = rows => {
-  let output
+  let output;
   rows.forEach((_, idx) => {
     const column = rows.map(row => row[idx])
-    if (column.every(el => el === column[0])) {
+    if (column[0] && column.every(el => el === column[0])) {
       output = {
         winner: column[0],
         type: "column",
@@ -37,7 +37,7 @@ const evaluateColumns = rows => {
 }
 
 const evaluateDiagonals = rows => {
-  if (rows.map((row, idx) => row[idx]).every(square => square === rows[0][0])) {
+  if (rows[0][0] && rows.map((row, idx) => row[idx]).every(square => square === rows[0][0])) {
     return {
       winner: rows[0][0],
       type: "diagonal",
@@ -45,7 +45,7 @@ const evaluateDiagonals = rows => {
     }
   }
   
-  if (rows[0][2] === rows[1][1] && rows[2][0] === rows[1][1]) {
+  if (rows[0][2] && rows[0][2] === rows[1][1] && rows[2][0] === rows[1][1]) {
     return {
       winner: rows[0][2],
       type: "diagonal",
